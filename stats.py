50e58b6e04b1e9d71facd1d24996744d812e7d1c
Yian Shang <arbitraryster@gmail.com>
2015-07-17 00:38:31 -0700

    Added some more calculations to repository stats, including most and least active users. Renamed some classes to be more descriptive

--- stats.py
+++ stats.py
@@ -14,7 +14,7 @@ app.config['DEBUG'] = True
 # TODO: config file this
 ghub = Github("b736b1b52904e45adfd58d076f2a6ef514fb7a5f")
 
-class MemberStats(object):
+class MemberCommitStats(object):
     def __init__(self, commits):
         self.number_of_commits = len(commits)
         self.total_additions = sum(map(lambda commit: commit.stats.additions, commits))
@@ -24,16 +24,23 @@ class MemberStats(object):
 class RepositoryStats(object):
     def __init__(self, repo_name):
         self.repo = ghub.get_repo(repo_name)
-        self.commits = {}
+        self.user_commits = None
         
-    def get_commits(self, since_time):
+    def load_commits(self, since_time):
         commits_per_user = defaultdict(list)
         for commit in self.repo.get_commits(since=since_time):
             commits_per_user[commit.committer.login].append(commit)
         for name in counts.keys():
-            commits_per_user[name] = MemberStats(counts[name]).__dict__
-        return commits_per_user
+            commits_per_user[name] = MemberCommitStats(counts[name]).__dict__
+            
+        self.user_commits = sorted(commits_per_user.items(), key=operator.itemgetter(1).number_of_commits)
+        return self.user_commits
         
+    def get_most_active(self, n):
+        self.user_commits[:n]
+    
+    def get_least_active(self, n):
+        self.user_commits[len(self.user_commits)-n:]
 
 @app.route('/_commits')
 def count_commits():
